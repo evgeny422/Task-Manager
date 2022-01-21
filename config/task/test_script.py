46c943863f9120f0107a393af51bb88f4c74f834
Evgeny Rudakov <fullhaoss42russ@gmail.com>
2022-01-21 16:11:12 +0700

    add  non-working script (

--- config/task/test_script.py
+++ config/task/test_script.py
@@ -1,1 +1,30 @@
 import requests
+
+url_auth = 'http://127.0.0.1:8000/api-auth/login/'
+url_task = 'http://127.0.0.1:8000/api/v1/task/'
+url_task_create = 'http://127.0.0.1:8000/api/v1/task/create'
+
+user_agent_val = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                 'Chrome/75.0.3770.142 Safari/537.36'
+
+with requests.Session() as s:
+    r = s.get(url_auth, headers={
+        'User-Agent': user_agent_val
+    })
+
+    s.headers.update({'Referer': url_auth})
+
+    s.headers.update({'User-Agent': user_agent_val})
+
+    _xsrf = s.cookies.get('_xsrf', domain="http://127.0.0.1:8000/")
+
+    post_request = s.post(url_auth, {
+
+        'username': 'root',
+        'password': 'root',
+        '_xsrf': _xsrf,
+
+    })
+
+    g = s.get(url_task).json
+    print(g)
