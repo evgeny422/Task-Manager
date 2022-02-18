ec94537eadc44907a3f8c92595d5ce4fb156c64c
polyerter <polyerter@tspu.edu.ru>
2022-02-18 10:40:18 +0700

    fix

--- config/task/api-script.py
+++ config/task/api-script.py
@@ -1,5 +1,87 @@
+from abc import abstractmethod
+
 import requests
-import schedule
+
+
+# import schedule
+
+
+class Data:
+    response: str
+    is_active: bool
+    package_id: int
+
+    @abstractmethod
+    def create(self, item):
+        pass
+
+    def get_message(self):
+        return {
+            'response': self.response,
+            'is_active': self.is_active,
+            'status': 1,
+            'package': self.package_id,
+        }
+
+
+class SuccessData(Data):
+    def create(self, item):
+        self.response = item['response']
+        self.is_active = item['is_active']
+        self.package_id = item['PackageID']
+
+        return self
+
+
+class ErrorData(Data):
+    def create(self, item):
+        return {
+            'response': item['RESPONSE'],
+            'is_active': False,
+            'status': 1,
+            'package': item['PackageID'],
+        }
+
+
+class Error2Data(Data):
+    def create(self, item):
+        return {
+            'response': item['RESPONSE'],
+            'is_active': False,
+            'status': 1,
+            'package': item['PackageID'],
+        }
+
+
+class Adapter:
+    def compare(self, response):
+        if response == 'success':
+            return SuccessData().create(response)
+        elif response == 'error':
+            return ErrorData().create(response)
+        elif response == 'error2':
+            return Error2Data().create(response)
+        elif response == 'error3':
+            return SuccessData().create(response)
+
+        raise Exception('Error')
+
+
+class RequestServer:
+    token = '7d4cc4bbef69cfdf27bf05cdf221ebbb98d2dbcd'
+    header = {
+        'User-Agent': 'api-script',
+        "Token-api": f'{token}',
+    }
+
+    def send(self, data: Data):
+        self.requerst(task_url + 'update/', data.get_message(), 'patch')
+
+    def requerst(self, url: str, data, request_type='post'):
+        if request_type == 'post':
+            requests.post(url, data, headers=self.headers)
+        if request_type == 'patch':
+            requests.patch(url, data, headers=self.headers)
 
 
 class ConnectionScript:
@@ -46,6 +128,11 @@ class ConnectionScript:
             # except requests.exceptions.RequestException as e:
             #     raise e
             #
+
+            adapter = Adapter().compare(package)
+            RequestServer.send(adapter)
+
+            #
             # if 'Error' in package:
             #     """Если получаем ошибку"""
             #
@@ -64,14 +151,14 @@ class ConnectionScript:
             #         'status': 1,
             #         'package': package['PackageID'],
             #     }, headers=self.header)
-
-            """Статус - выполнено + response"""
-            requests.patch(task_url + 'update/', data={
-                'response': 'done',
-                # 'is_active': False,
-                'status': 1,
-                # 'package' : package,
-            }, headers=self.header)
+            #
+            # """Статус - выполнено + response"""
+            # requests.patch(task_url + 'update/', data={
+            #     'response': 'done',
+            #     # 'is_active': False,
+            #     'status': 1,
+            #     # 'package' : package,
+            # }, headers=self.header)
 
             task_queue.clear()
 
